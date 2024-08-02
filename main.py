774ee2a0e06b1ce8b52669400a81fe4a8b9aeb05
mmoua89 <mmoua.89@outlook.com>
2024-08-02 15:49:03 -0700

    add determine 'AM' or 'PM' and 12-hour clock

--- main.py
+++ main.py
@@ -5,22 +5,29 @@ Assignment: Module 3, Part 2
 """
 
 def main():
-    time_now = input('Please enter the current time in hour in the range of 0 to 23:\n')
-    hour_wait = input('Please enter the number of hours greater than 0 to wait for the alarm:\n')
+    time_now = input('Please enter the (current time) from 0 to 24:\n')
+    hour_wait = input('Please enter the (wait time) greater than or equal to 0:\n')
     if not valid_number(time_now) or not valid_number(hour_wait):
-        print('Current time or wait time in hour is not a valid number.')
+        print('The (current time) or (wait time) is not a valid number.')
         return
 
-    if int(time_now) < 0 or int(time_now) > 23:
-        print('The current time is not in the range of 0 and 23. Please try again.')
+    if int(time_now) < 0 or int(time_now) > 24:
+        print('Please try again. The (current time) is not in the range of 0 to 23.')
         return
 
     if int(hour_wait) < 0:
-        print('The wait time in hour(s) cannot be less than 0. Please try again.')
+        print('Please try again. The (wait time) in hours cannot be less than 0.')
         return
 
-    alarm_time = get_alarm_time(time_now, hour_wait)
-    print('The alarm will ring at {}:00 {}'.format(alarm_time, 'AM' if alarm_time < 12 else 'PM'))
+    # get the alarm time in 24-hour clock
+    alarm_time_24_hr = get_alarm_time(time_now, hour_wait)
+    # determine morning 'AM' or afternoon 'PM' time
+    am_or_pm = get_am_or_pm(alarm_time_24_hr)
+    # calculate the alarm time in 12-hour clock
+    alarm_time_12_hr = alarm_time_24_hr % 12
+
+    print('The alarm will ring at {}:00 {} ({}:00 {})'.format(
+        alarm_time_24_hr, am_or_pm, alarm_time_12_hr, am_or_pm))
 
 def get_alarm_time(time_now, hour_wait) -> int:
     """
@@ -30,6 +37,14 @@ def get_alarm_time(time_now, hour_wait) -> int:
     :return: the alarm time
     """
     return (int(time_now) + int(hour_wait)) % 24
+
+def get_am_or_pm(time) -> str:
+    """
+    Determine the morning or afternoon time
+    :param time: user time in hour
+    :return: 'AM' or 'PM'
+    """
+    return 'AM' if time < 12 else 'PM'
 
 def valid_number(num) -> bool:
     """
